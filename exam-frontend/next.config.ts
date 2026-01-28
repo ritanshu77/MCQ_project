73b2931fa06c3858c324a4d0a948f5103e3b8a37
ritanshupatwa <rpchhipa@gmail.com>
2026-01-28 14:31:48 +0530

    Fix: root: __dirname

--- exam-frontend/next.config.ts
+++ exam-frontend/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
   // Try root level first (Next.js 15.2.3+)
   allowedDevOrigins: ['localhost','172.19.160.1'],
   
