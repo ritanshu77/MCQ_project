9ed78ddf4532bca88c7d59bce8e9325b73e09930
ritanshupatwa <rpchhipa@gmail.com>
2026-02-02 14:45:45 +0530

    admin ts config change

--- exam-admin-panel/vite.config.ts
+++ exam-admin-panel/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/admin/',
+  base: '/admin',
   server: {
     port: 3003,
     host: true
